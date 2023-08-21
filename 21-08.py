bee6bb8e7d126e94a9bc731a0b5e92f7b9d93a62
manish04042003 <manishkumar817835@gmail.com>
2023-08-21 14:15:37 +0530

    Q). Area of Circle

--- 21-08.py
+++ 21-08.py
@@ -1,7 +1,15 @@
-a = eval(input("Enter the First number: " ))
-b = eval(input("Enter the second number: "))
-print("Sum of Given nnumbers are :", (a + b))
+import math
 
+
+# a = eval(input("Enter the First number: " ))
+# b = eval(input("Enter the second number: "))
+# print("Sum of Given nnumbers are :", (a + b))
+
+
+# --------------------------------------Area of Circule-----------------------------------------------------
+
+r= int(input("Enter the Radius in cm : "))
+print((math.pi)*r*r);
 
 
 
