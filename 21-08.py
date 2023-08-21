ecdb0c2b32604a0d50025da259871a482d3f0df1
manish04042003 <manishkumar817835@gmail.com>
2023-08-21 14:07:44 +0530

    first commit

---
+++ 21-08.py
@@ -0,0 +1,9 @@
+a = eval(input("Enter the First number: " ))
+b = eval(input("Enter the second number: "))
+print("Sum of Given nnumbers are :", (a + b))
+
+
+
+
+# Case sensitive 
+# Indendation ==== spaces shoud be align if there is any unexpexted space its give a error of INDENDATION
