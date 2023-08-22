89008d62fa39292ea7b2f2080742f98b4a4e6652
manish04042003 <manishkumar817835@gmail.com>
2023-08-22 10:12:20 +0530

    HiFive Code

--- 22-08.py
+++ 22-08.py
@@ -1,4 +1,7 @@
-# if statment 
+# ----------------------------------------if statements --------------------------------------
+# if(True):
+#     print("this statment is true");
 
-if(True):
-    print("this statment is true");
+num = eval(input("Enter a number : "));
+if(num%5==0):
+    print('hiFive')
