6c344c54c1e6a934ca8cd54182c8bc28bd98d3b3
manish04042003 <manishkumar817835@gmail.com>
2023-08-22 10:37:17 +0530

    Grade system

--- 22-08.py
+++ 22-08.py
@@ -11,9 +11,24 @@
 # ----------------------------------------if else statements --------------------------------------
 # Odd even
 
-num = eval(input("Enter a number : "));
-if(num%2==0):
-    print('Even');
+# num = eval(input("Enter a number : "));
+# if(num%2==0):
+#     print('Even');
+# else:
+#     print('Odd');
+
+
+# ---------------------------------------- nested if else statements --------------------------------------
+score = eval(input("enter the first number"))
+
+if(score>90):
+    print('F Grade');
+elif(score>80):
+    print('B Grade');
+elif(score>70):
+    print('c Grade');
+elif(score>60):
+    print('D Grade');
 else:
-    print('Odd');
+    print('F Grade')
     
