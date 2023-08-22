849623d28c114da5dea5c3c0c37fd21a575be760
manish04042003 <manishkumar817835@gmail.com>
2023-08-22 10:42:23 +0530

    First loop program in pyton

--- 22-08.py
+++ 22-08.py
@@ -19,16 +19,22 @@
 
 
 # ---------------------------------------- nested if else statements --------------------------------------
-score = eval(input("enter the first number"))
+# score = eval(input("enter the first number"))
 
-if(score>90):
-    print('F Grade');
-elif(score>80):
-    print('B Grade');
-elif(score>70):
-    print('c Grade');
-elif(score>60):
-    print('D Grade');
-else:
-    print('F Grade')
+# if(score>90):
+#     print('F Grade');
+# elif(score>80):
+#     print('B Grade');
+# elif(score>70):
+#     print('c Grade');
+# elif(score>60):
+#     print('D Grade');
+# else:
+#     print('F Grade')
     
+
+# ---------------------------------------- Loops --------------------------------------
+count = 0;
+while(count<10):
+      print("Programing is Fun");
+      count = count +1 ;
