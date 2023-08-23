0007599e03a65c3db64af69a6253e0ba4c95facf
manish04042003 <manishkumar817835@gmail.com>
2023-08-23 10:58:55 +0530

    prime nukmber btw 2 numbers

--- 22-08.py
+++ 22-08.py
@@ -54,15 +54,34 @@
 
 
 # Prime or Not 
-a = eval(input("enter the first number "));
-count = 2;
-while(count<a):
-    if(a%count==0):
-        print("not a Prime Number");
-        break;
-    count= count +1;
-else:
-    print("is a Prime Number");
+# a = eval(input("enter the first number "));
+# count = 2;
+# while(count<a):
+#     if(a%count==0):
+#         print("not a Prime Number");
+#         break;
+#     count= count +1;
+# else:
+#     print("is a Prime Number");
+
+
+start = eval(input('Enter the strating number : '))
+end = eval(input('Enter the Ending number : '))
+lastPrime = 0;
+while(start<=end):
+    i = 2;
+    while(i<start):
+        if(start%i==0):
+            break
+        i=i+1;
+    else:
+        if(lastPrime ==(start-2) or lastPrime == 0):
+            lastPrime = start
+            print(lastPrime);
+    start = start +1 ;
+
+print("ennd")
+
     
 
 
