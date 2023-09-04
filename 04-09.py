fc4619ec569a82e72bca44092f2ee03e4b6083d9
manish04042003 <manishkumar817835@gmail.com>
2023-09-04 14:10:42 +0530

    ncr

--- 04-09.py
+++ 04-09.py
@@ -21,5 +21,24 @@
 # print(power);
 
 # ---------------------------------------Comprhension
-power1 = [2 ** i for i in range(1,10) if i>5]
-print(power1)
+# power1 = [2 ** i for i in range(1,10) if i>5]
+# print(power1)
+
+# mat = [x+y for x in ['a','b'] for y in ['c','d']];
+
+# print('a' not in mat)
+
+n= eval(input('emter the value of N : '))
+r= eval(input('emter the value of R : '))
+
+def fact(n):
+  ans = 1;
+  while(n>0):
+        ans = ans * n;
+        n=  n-1 ;
+  return ans  ;
+
+
+ncr = fact(n)/fact(n-r);
+
+print(ncr)
